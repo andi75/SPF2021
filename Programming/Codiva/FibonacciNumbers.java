0873411821c7eb2fbb7d825e89a052063c316969
Andreas Umbach <umbach@gmail.com>
2022-02-03 11:13:25 +0100

    added Fibonacci, missing ForAndWhileLoops Code and fixed some small details

---
+++ Programming/Codiva/FibonacciNumbers.java
@@ -0,0 +1,16 @@
+public class FibonacciNumbers {
+    public static void main(String[] argv)
+    {
+        int current = 1;
+        int last = 1;
+        System.out.print(last);
+        while(current < 1000)
+        {
+            System.out.print(", " + current);
+            int next = current + last;
+            last = current;
+            current = next;
+        }
+        System.out.println(); // print a new line
+    }
+}
