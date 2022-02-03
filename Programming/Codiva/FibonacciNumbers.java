b1d82003163ffc6c75ad2464dfea69b8fccab5d5
Andreas Umbach <umbach@gmail.com>
2022-02-03 11:33:05 +0100

    started at 0

--- Programming/Codiva/FibonacciNumbers.java
+++ Programming/Codiva/FibonacciNumbers.java
@@ -2,8 +2,7 @@ public class FibonacciNumbers {
     public static void main(String[] argv)
     {
         int current = 1;
-        int last = 1;
-        System.out.print(last);
+        int last = 0;
         while(current < 1000)
         {
             System.out.print(", " + current);
